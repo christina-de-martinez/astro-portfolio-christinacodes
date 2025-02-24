3c98f2f0ee2163bee56afdbf6a4e86506ff2f9db
christinamartinez <christina.martinez@faithlife.com>
2025-02-23 23:20:33 -0800

    Try adding resend (server action isn't working...)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
+
     integrations: [
         mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: false
         })
-    ]
+    ],
+
+    adapter: netlify()
 });
